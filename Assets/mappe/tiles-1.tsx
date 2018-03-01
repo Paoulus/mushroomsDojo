2c886e6f6d1ce708025f2528a93e71633e1108a3
Alberto <ainter21.6@gmail.com>
2018-03-01 11:06:26 +0100

    Resolved bug in tilemap

--- Assets/mappe/tiles-1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tiles-1" tilewidth="16" tileheight="16" tilecount="68" columns="17">
- <image source="tiles-1.png" width="272" height="64"/>
-</tileset>
--- js/mushrooms-game.js
+++ js/mushrooms-game.js
@@ -6,17 +6,23 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv',
 	});
 
 function preload(){
+	game.load.tilemap('level-1','Assets/mappe/tilemap600x20.json',null,Phaser.Tilemap.TILED_JSON);
+	game.load.image('tiles-1','Assets/mappe/tiles-1.png');
 	game.load.atlasJSONArray('idle','Assets/atlas_idle.png','Assets/atlas_idle.json');
 	game.load.atlasJSONArray('run','Assets/atlas_run.png','Assets/atlas_run.json');
 	game.load.atlasJSONArray('jump','Assets/atlas_jump.png','Assets/atlas_jump.json');
 	game.load.atlasJSONArray('dead','Assets/atlas_dead.png','Assets/atlas_dead.json');
 }
+var map;
 
 function create(){
 	robot = game.add.sprite(150,20,'run','Run (1).png');
 	robot.scale.setTo(0.1,0.1);
 
 	game.physics.enable(robot,Phaser.Physics.ARCADE);
+	map = game.add.tilemap('level-1');
+	map.addTilesetImage('tiles-1');
+	map.setCollisionByExclusion([ 13, 14, 15, 16, 17, 46, 47, 48, 49, 50, 51 ]);
 
 	//adding animations using frames obtained from the atlases
 	//the generateFrameNames produces the names for the single frames; 
