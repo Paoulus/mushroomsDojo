bd168469adb8c42b5615bafc7f443ac50c65d199
Alberto <ainter21.6@gmail.com>
2018-03-08 17:53:27 +0100

    Add slime sprite in tiled map

---
+++ Assets/spritesheets/slime.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="slime" tilewidth="44" tileheight="34" tilecount="2" columns="1">
+ <image source="slime.png" width="44" height="68"/>
+</tileset>
