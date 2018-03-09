a38d2e3f6eebcba4438cbd45a690ee24db0d0a76
Paolo <paolo.chiste-2@studenti.unitn.it>
2018-03-09 12:26:45 +0100

    deleted old Asset folder

--- Assets/mappe/slime.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="slime" tilewidth="44" tileheight="32" tilecount="2" columns="2">
- <image source="../spritesheets/slime.png" width="88" height="32"/>
-</tileset>
